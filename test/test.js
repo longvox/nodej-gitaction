5bbaca565fcbd7dcef58813e2bb9388306f65b13
Long Vo <longvox98@gmail.com>
2021-04-08 20:13:32 +0700

    test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,12 @@
 const request = require("supertest");
 const app = require("../index");
 
+// eslint-disable-next-line no-undef
 describe("GET /", () => {
+  // eslint-disable-next-line no-undef
   it("respond with Hello World via Github Actions", (done) => {
-    request(app).get("/").expect("Hello World via Github Actions test again", done);
-  })
+    request(app)
+      .get("/")
+      .expect("Hello World via Github Actions", done);
+  });
 });
